2bdb538c100cac7d2e07060b2e349eb7646a2dc3
Omkar4033 <91587106+Omkar4033@users.noreply.github.com>
2023-02-18 19:50:13 +0530

    without piechart implementation

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
 import React, { useState, useEffect } from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { WiVolcano } from "react-icons/wi";
+import { MdOutlineLocalFireDepartment , MdWaterDrop,MdOutlineStorm} from "react-icons/md";
 import Infobox from "./Components/Infobox";
 import Map, { Marker } from "react-map-gl";
 import Loader from "./Components/Loader";
 import { PieChart } from "react-minimal-pie-chart";
 const App = () => {
   const [data, setData] = useState([]);
-  const [infobox, setinfobox] = useState({ id: null, title: null, url: null,category:null });
+  const [infobox, setinfobox] = useState();
   const [loader, setloader] = useState(false);
+
+  // let wildfire=0,volcano=0,storms=0,other=0;
+
+  // const countall=(d)=>{
+  //     if(d.categories[0].id ===12)
+  //     volcano++;
+  // }
 
   useEffect(() => {
     const getValue = async () => {
       setloader(true);
       const res = await fetch(
-        "https://eonet.gsfc.nasa.gov/api/v2.1/categories/12"
+        "https://eonet.gsfc.nasa.gov/api/v2.1/events"
       );
       const resdata = await res.json();
       setData(resdata.events);
       console.log(data);
       setloader(false);
+      // const rest=await data.filter(countall);
     };
 
     getValue();
     // eslint-disable-next-line
   }, []);
+
+  // console.log("total data is",data.length);
+  // console.log(volcano);
 
   return (
     <div>
@@ -33,17 +45,16 @@ const App = () => {
       ) : (
         <div className="main">
           <div className="rightbox">
-            {/* <div className="navbar">
-      Volcano Tracker
-    </div>  */}
+            <div className="navbar">Disaster Tracker</div>
             {infobox && <Infobox infobox={infobox} />}
             <div className="calamties_chart">
-              <label> Chart</label>
+             
               <PieChart
                 data={[
-                  { text: "One", value: 10, color: "#FEDEFF",fontColor: "black" },
-                  { title: "Two", value: 15, color: "#93C6E7" },
-                  { title: "Three", value: 20, color: "#B9F3FC" },
+                  { text: "One", value: 80, color: "tomato" },
+                  { title: "Two", value: 15, color: "pink" },
+                  { title: "Four", value: 14, color: "aqua" },
+                  { title: "Three", value: 1, color: "purple" },
                 ]}
               />
               ;
@@ -60,24 +71,63 @@ const App = () => {
             mapStyle="mapbox://styles/mapbox/streets-v9"
             mapboxAccessToken="pk.eyJ1Ijoib21rYXI0MDMzIiwiYSI6ImNsZTE3aDVqNjBlMG0zcW12MzZlMm9hcDUifQ.5PArT3vGqGuNpM0_yQay1w"
           >
-            {data.map((d) => (
+            {   data.map((d) => (
               <Marker
                 key={d.id}
                 longitude={d.geometries[0].coordinates[0]}
                 latitude={d.geometries[0].coordinates[1]}
                 anchor="bottom"
               >
-                <WiVolcano
+               { d.categories[0].id ===12 && <WiVolcano
+                  onClick={() =>
+                    setinfobox({
+                      id: d.id,
+                      title: d.title,
+                      category_id: d.categories[0].id,
+                      url: d.sources[0].url,
+                      category:d.categories[0].title
+                    })
+                  }
+                  style={{ fontSize: "25px", color: "yello" }}
+                />}
+               { d.categories[0].id ===8 && <MdOutlineLocalFireDepartment
                   onClick={() =>
                     setinfobox({
                       id: d.id,
                       title: d.title,
                       url: d.sources[0].url,
+                      category_id: d.categories[0].id,
                       category:d.categories[0].title
                     })
                   }
-                  style={{ fontSize: "25px", color: "red" }}
-                />
+                  style={{ fontSize: "15px", color: "tomato" }}
+                />}
+               { d.categories[0].id ===15 && <MdWaterDrop
+                  onClick={() =>
+                    setinfobox({
+                      id: d.id,
+                      title: d.title,
+                      url: d.sources[0].url,
+                      category_id: d.categories[0].id,
+                      category:d.categories[0].title
+                    })
+                  }
+                  style={{ fontSize: "15px", color: "#00FFF6" }}
+                />}
+               { d.categories[0].id ===10 && <MdOutlineStorm
+                  onClick={() =>
+                    setinfobox({
+                      id: d.id,
+                      title: d.title,
+                      url: d.sources[0].url,
+                      category_id: d.categories[0].id,
+                      category:d.categories[0].title
+                    })
+                  }
+                  style={{ fontSize: "15px", color: "purple" }}
+                />}
+             
+             
               </Marker>
             ))}
           </Map>
--- src/Components/Infobox.js
+++ src/Components/Infobox.js
@@ -5,10 +5,11 @@ const Infobox = ({ infobox }) => {
     <div className="infobox">
       <h4>Event Info</h4>
       <ul >
-        <li  >Id: {infobox.id}</li>
-        <li>Title: {infobox.title}</li>
-        <li>Category: {infobox.category}</li>
-        <li>
+        <li><label>Category_Id</label>{infobox.category_id}</li>
+        <li><label>Event_Id</label> {infobox.id}</li>
+        <li><label>Location</label> {infobox.title}</li>
+        <li ><label>Category</label> {infobox.category}</li>
+        <li style={{"textAlign":"center"}}>
           <a rel="noreferrer" target="_blank" href={infobox.url}>read more</a>
         </li>
       </ul>
